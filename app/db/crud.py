7af45078b40a40698e16b72f3775dee12d42308a
Игорь <idemenko22@gmail.com>
2026-02-19 15:43:28 +1000

    added crud

---
+++ app/db/crud.py
@@ -0,0 +1,103 @@
+from sqlalchemy.orm import Session
+import datetime
+
+from app.db.database import SessionLocal
+from app.db.models import User, Request, Payment, Data_Plan, Status_Pay
+
+def create_user(tg_id: int, username: int, db: Session):
+    user_db = User(tg_id=tg_id, username=username)
+    db.add(user_db)
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+def get_user_tg_id(tg_id: int, db: Session):
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    return user_db
+
+def update_user_tg_id(tg_id: int,
+                      db: Session,
+                      username: str = None,
+                      data_plan: Data_Plan = None,
+                      subscription_expires: datetime = None,
+                      total_request: int = None,
+                      monthly_request: int = None,
+                      is_active: bool = None):
+
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    if not username is None:
+        user_db.username = username
+
+    if not data_plan is None:
+        user_db.data_plan = data_plan
+
+    if not subscription_expires is None:
+        user_db.subscription_expires = subscription_expires
+
+    if not total_request is None:
+        user_db.total_request = total_request
+
+    if not monthly_request is None:
+        user_db.monthly_request = monthly_request
+
+    if not is_active is None:
+        user_db.is_active = is_active
+
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+def update_user_data_plan(tg_id: int, data_plan: Data_Plan, subscription_expires: datetime, db: Session):
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    user_db.data_plan = data_plan
+    user_db.subscription_expires = subscription_expires
+
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+def update_user_add_request(tg_id: int, db: Session):
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    user_db.total_request = user_db.total_request + 1
+    user_db.monthly_request = user_db.monthly_request + 1
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+def update_user_clear_request(tg_id: int, db: Session):
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    user_db.monthly_request = 0
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+def update_user_is_active(tg_id: int, db: Session):
+    user_db = db.query(User).filter(User.tg_id == tg_id).first()
+    user_db.is_active = not user_db.is_active
+    db.commit()
+    db.refresh(user_db)
+    return user_db
+
+
+
+def create_request(user_id: int, request: str, db: Session):
+    request_db = Request(user_id=user_id, request=request)
+    db.add(request_db)
+    db.commit()
+    db.refresh(request_db)
+    return request_db
+
+def get_request(id: int, db: Session):
+    request_db = db.query(Request).filter(Request.id == id).first()
+    return request_db
+
+def create_payment(provider_payment_id: int, user_id: int, amount: int, status: Status_Pay, db: Session):
+    payment_db = Payment(provider_payment_id=provider_payment_id, user_id=user_id, amount=amount, status=status)
+    db.add(payment_db)
+    db.commit()
+    db.refresh(payment_db)
+    return payment_db
+
+def get_payment(id: int, db: Session):
+    payment_db = db.query(Payment).filter(Payment.id == id).first()
+    return payment_db
