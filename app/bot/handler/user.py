1344cffdbbe6454ed707de37d48486684990f086
Игорь <idemenko22@gmail.com>
2026-02-20 12:58:30 +1000

    added main panel

--- app/bot/handler/user.py
+++ app/bot/handler/user.py
@@ -1,13 +1,15 @@
 from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 
 from sqlalchemy.orm import Session
 import logging
 
 from app.bot.static import RegisterUser
+from app.bot.keyboard import main_kb, category_kb
 from app.db import crud
+from app.config.config import CATEGORY
 
 logger = logging.getLogger(__name__)
 user_router_bot = Router()
@@ -26,7 +28,37 @@ async def get_username(message: Message, state: FSMContext, db: Session):
     await state.clear()
     logger.info("User success register")
     await message.answer("Вы успешно зарегистрированны")
+    await to_main(message)
 
-@user_router_bot.message(Command("/main"))
+
+############################################################################################################################
+################################################ main ######################################################################
+############################################################################################################################
+@user_router_bot.message(Command("main"))
 async def to_main(message: Message):
-    await message.answer("Main panel")
+    await message.answer("Main panel", reply_markup=await main_kb())
+
+
+############################################### generate text ##############################################################
+@user_router_bot.message(F.text.casefold().endswith("генерация текста"))
+async def text_generate_handler(message: Message):
+    await message.answer("Выберите категорию в которой хотите продать товар:", reply_markup=await category_kb())
+
+@user_router_bot.callback_query(F.data.startswith("car"))
+async def text_generate_get_category(callback: CallbackQuery):
+    await callback.message.answer("Выбрана категория автомобили")
+
+
+
+############################################### profil #####################################################################
+
+
+
+############################################### data plan ##################################################################
+
+
+
+
+############################################################################################################################
+############################################################################################################################
+############################################################################################################################
