434d333730622d3b9d64a56227545c42b4d2b26d
Игорь <idemenko22@gmail.com>
2026-02-20 11:29:05 +1000

    added func register

---
+++ app/bot/handler/user.py
@@ -0,0 +1,30 @@
+from aiogram import Router, F
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+from sqlalchemy.orm import Session
+
+from app.bot.static import RegisterUser
+from app.db import crud
+
+
+user_router_bot = Router()
+
+@user_router_bot.message(Command("register"))
+async def register(message: Message, state: FSMContext):
+    await message.answer("Регистрация")
+    await message.answer("Напиши свой username: ")
+    await state.set_state(RegisterUser.username)
+
+@user_router_bot.message(RegisterUser.username)
+async def get_username(message: Message, state: FSMContext, db: Session):
+    tg_id = message.from_user.id
+    username = message.text
+    crud.create_user(tg_id, username, db)
+    await state.clear()
+    await message.answer("Вы успешно зарегистрированны")
+
+@user_router_bot.message(Command("/main"))
+async def to_main(message: Message):
+    await message.answer("Main panel")
