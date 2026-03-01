d6532ac1596d0321b83bdfda2a19c279ed4fdc23
Игорь <idemenko22@gmail.com>
2026-03-01 22:05:14 +1000

    added new jobs added new handler

--- app/scheduler/jobs.py
+++ app/scheduler/jobs.py
@@ -1,6 +1,4 @@
 from aiogram import Bot
-from sqlalchemy.orm import Session
-from datetime import datetime
 import logging
 
 from app.db import crud
@@ -28,3 +26,34 @@ async def check_subscriptions(bot: Bot):
     finally:
         db.close()
         logger.info("Finished check subscription")
+
+
+async def reset_monthly_requests():
+    logger.info("Start reset monthly requests")
+    db = SessionLocal()
+    try:
+        users_data = crud.get_users(db)
+        for user_data in users_data:
+            try:
+                user_id = user_data.tg_id
+                crud.update_user_clear_request(user_id, db)
+            except Exception:
+                logger.error("Error in reset monthly request user_id=%s", user_id, exc_info=True)
+    finally:
+        db.close()
+        logger.info("Finished reset monthly requests")
+
+async def reset_daily_requests():
+    logger.info("Start reset daily requests")
+    db = SessionLocal()
+    try:
+        users_data = crud.get_users(db)
+        for user_data in users_data:
+            try:
+                user_id = user_data.tg_id
+                crud.update_user_clear_request_daily(user_id, db)
+            except Exception:
+                logger.error("Error in reset daily request user_id=%s", user_id, exc_info=True)
+    finally:
+        db.close()
+        logger.info("Finished reset daily requests")
--- main.py
+++ main.py
@@ -13,20 +13,42 @@ from app.bot.handler.user import user_router_bot
 from app.bot.handler.payments import payment_router_bot
 from app.bot.keyboard import register_kb
 from app.config.logging_config import setup_logging
-from app.scheduler.jobs import check_subscriptions
+from app.scheduler.jobs import check_subscriptions, reset_monthly_requests, reset_daily_requests
 
 
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("<i>AvitoTextBot</i>\n<b>Зарегистрируйся Бабуин...</b>", parse_mode="HTML", reply_markup=await register_kb())
+    await message.answer(
+        "<i>AvitoTextBot</i>\n"
+        "Для начала работы зарегистрируйся",
+        parse_mode="HTML",
+        reply_markup=await register_kb()
+    )
+
+
+@dp.message(Command("help"))
+async def cmd_help(message: Message):
+    await message.answer(
+        "<i>AvitoTextBot</i>\n"
+        "<b>Commands:</b>\n"
+        "1) /start - Запуск бота\n"
+        "2) /register - Регистрация\n"
+        "3) /main - На главную\n"
+        "4) /help - Помощь",
+        parse_mode="HTML",
+        reply_markup=await register_kb()
+    )
 
 
 async def set_my_command(bot: Bot):
     command = [
         BotCommand(command="/start", description="Запуск AvitoBot"),
-        BotCommand(command="/main", description="На главную")
+        BotCommand(command="/register", description="Регистрация"),
+        BotCommand(command="/main", description="На главную"),
+        BotCommand(command="/help", description="Помощь")
     ]
     await bot.set_my_commands(command)
+
 
 async def start_bot():
     await set_my_command(bot)
@@ -34,13 +56,27 @@ async def start_bot():
     dp.include_router(payment_router_bot)
     await dp.start_polling(bot)
 
+
 async def start_scheduler():
     scheduler = AsyncIOScheduler(timezone="Asia/Vladivostok")
 
     scheduler.add_job(
         check_subscriptions,
-        trigger=IntervalTrigger(seconds=60*60),
+        trigger=IntervalTrigger(seconds=60 * 60),
         kwargs={"bot": bot}
+    )
+    scheduler.add_job(
+        reset_monthly_requests,
+        trigger="cron",
+        day=1,
+        hour=0,
+        minute=0
+    )
+    scheduler.add_job(
+        reset_daily_requests,
+        trigger="cron",
+        hour=0,
+        minute=0
     )
 
     scheduler.start()
@@ -51,6 +87,7 @@ async def main():
         start_bot(),
         start_scheduler()
     )
+
 
 if __name__ == "__main__":
     setup_logging()
