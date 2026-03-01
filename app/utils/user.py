dc6f0371663136a2a9c2efaa85a150a41913ab4e
Игорь <idemenko22@gmail.com>
2026-03-01 22:06:01 +1000

    move func activate_sub to servise_user

--- app/utils/user.py
+++ app/utils/user.py
@@ -8,19 +8,3 @@ from app.db import crud
 
 
 logger = logging.getLogger(__name__)
-
-async def activate_subscription(user_id: int, plan: Data_Plan, db: Session):
-    logger.info("Activating subscription user_id=%s plan=%s", user_id, plan)
-    user_data = crud.get_user_tg_id(user_id, db)
-    if user_data.date_plan == plan:
-        subscription_expires = user_data.subscription_expires + timedelta(days=30)
-
-        logger.info("DB request update_plan user_id=%s", user_id)
-        crud.update_user_data_plan(user_id, plan, subscription_expires, db)
-        logger.info("DB successful response update_plan user_id=%s", user_id)
-
-    subscription_expires = datetime.now() + timedelta(days=30)
-
-    logger.info("DB request update_plan user_id=%s", user_id)
-    crud.update_user_data_plan(user_id, plan, subscription_expires, db)
-    logger.info("DB successful response update_plan user_id=%s", user_id)
