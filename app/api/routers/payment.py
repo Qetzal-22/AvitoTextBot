dcb5637f5a6ef332e5a75c4680678deedda4a044
Игорь <idemenko22@gmail.com>
2026-03-05 13:37:25 +1000

    feat(router): add /payments endpoint

--- app/api/routers/payment.py
+++ app/api/routers/payment.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.api.templates import templates
+from app.services import finance_service
 
 payment_router_api = APIRouter(prefix="/payments", tags=["payments"])
 logger = logging.getLogger(__name__)
 
 @payment_router_api.get("/")
 def get_payments(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("payments.html", {"request": request})
+    payments = finance_service.get_payments(db)
+    payments_data = enumerate(payments)
+    return templates.TemplateResponse("payments.html", {"request": request, "payments": payments_data})
