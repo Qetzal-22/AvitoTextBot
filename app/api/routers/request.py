b9b05565dc058781204be4dc687c500c22146fed
Игорь <idemenko22@gmail.com>
2026-03-06 17:16:50 +1000

    feat(routers): add /requests endpoint

--- app/api/routers/request.py
+++ app/api/routers/request.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import Session
 
 from app.db.database import get_db
 from app.api.templates import templates
+from app.services import request_service
 
 request_router_api = APIRouter(prefix="/requests", tags=["requests"])
 logger = logging.getLogger(__name__)
 
 @request_router_api.get("/")
 def get_request(request: Request, db: Session = Depends(get_db)):
-    return templates.TemplateResponse("requests.html", {"request": request})
+    requests = request_service.get_requests(db)
+    requests_data = enumerate(requests)
+    return templates.TemplateResponse("requests.html", {"request": request, "requests": requests_data})
