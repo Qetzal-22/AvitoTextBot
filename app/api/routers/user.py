72d9935582df819320f9b5b00e03cc6545227e07
Игорь <idemenko22@gmail.com>
2026-03-10 19:48:18 +1000

    feat(router): add /users/{id} endpoint

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -14,3 +14,20 @@ def get_users(request: Request, db: Session = Depends(get_db)):
     users = user_service.get_users(db)
     data_users = enumerate(users)
     return templates.TemplateResponse("users.html", {"request": request, "users": data_users})
+
+@user_router_api.get("/{id}")
+def get_user(request: Request, db: Session = Depends(get_db), id: int = None):
+    user = user_service.get_user_dict_form(db, id)
+    view_data = {
+        "id": "ID",
+        "tg_id": "Telegram ID",
+        "username": "Username",
+        "data_plan": "Data plan",
+        "subscription_expires": "Subscription expires",
+        "total_request": "Total request",
+        "monthly_request": "Monthly request",
+        "daily_request": "Daily request",
+        "is_active": "Is active",
+        "create_at": "Create at"
+    }
+    return templates.TemplateResponse("user.html", {"request": request, "user": user, "view_data": view_data})
