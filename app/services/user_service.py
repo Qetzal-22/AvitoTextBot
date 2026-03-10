7e8f5e4d6c1b7422555622bb59ff7af6d66d8202
Игорь <idemenko22@gmail.com>
2026-03-10 19:56:18 +1000

    feat(services): add function get_user_dict_format

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 
 from app.db import crud
+from app.db.database import SessionLocal
 from app.db.models import Data_Plan
 
 logger = logging.getLogger(__name__)
@@ -19,6 +20,33 @@ def get_user(db: Session, user_id: int):
     users = crud.get_user_tg_id(user_id, db)
     logger.info("DB successful response get_user")
     return users
+
+def get_user_by_id(db: Session, user_id: int):
+    logger.info("DB request get_user")
+    users = crud.get_user(user_id, db)
+    logger.info("DB successful response get_user")
+    return users
+
+def get_user_dict_form(db: Session, user_id: int):
+    logger.info("DB request get_user")
+    user = crud.get_user(user_id, db)
+    logger.info("DB successful response get_user")
+    user_dict = {}
+    user_dict["id"] = user.id
+    user_dict["tg_id"] = user.tg_id
+    user_dict["username"] = user.username
+    user_dict["data_plan"] = user.data_plan
+    user_dict["subscription_expires"] = user.subscription_expires
+    user_dict["total_request"] = user.total_request
+    user_dict["monthly_request"] = user.monthly_request
+    user_dict["daily_request"] = user.daily_request
+    user_dict["is_active"] = user.is_active
+    user_dict["create_at"] = user.create_at
+    user_dict["requests"] = user.requests
+    user_dict["payments"] = user.payments
+    user_dict["count_requests"] = len(user.requests)
+    user_dict["count_payments"] = len(user.payments)
+    return user_dict
 
 async def disable_subscription(db: Session, user_id: int):
     logger.info("Disable subscription user_id=%s", user_id)
